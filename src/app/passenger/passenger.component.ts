d5ebcad71650916b06a2c654bab4c554a97e8513
Jano Siccardi <janosiccardi99@gmail.com>
2022-04-26 18:17:20 -0300

    - nuevo metodo save para crear o modificar pasajero

--- src/app/passenger/passenger.component.ts
+++ src/app/passenger/passenger.component.ts
@@ -8,11 +8,9 @@ import { PassengerService } from '../services/passenger.service';
   styleUrls: ['./passenger.component.css']
 })
 export class PassengerComponent implements OnInit {
-  id = 0;
-  passenger: PassengerModel = {
-    idPassenger: 0,
-    name: "Ejemplo"
-  };
+  id: number = 0;
+  name: string =  "Nombre";
+  passenger: PassengerModel;
   constructor(public passengerService : PassengerService) { 
   }
 
@@ -22,7 +20,19 @@ export class PassengerComponent implements OnInit {
   search(){
     this.passengerService.getPassenger(this.id).subscribe(data => {
       this.passenger = data;
+      this.id = data.idPassenger;
+      this.name = data.name;
     });
+  }
+
+  save(){
+    this.passenger.idPassenger = this.id;
+    this.passenger.name = this.name;
+    this.passengerService.savePassenger(this.passenger).subscribe(data => {
+      this.passenger = data;
+      this.search();
+    });
+    
   }
 
 }
--- src/app/services/passenger.service.ts
+++ src/app/services/passenger.service.ts
@@ -1,16 +1,26 @@
 import { PassengerModel } from "src/model/passenger.model";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient,HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
+const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+};
 @Injectable({
     providedIn: "root"
   })
 export class PassengerService{ 
     url: string = 'https://grops-backend-dnj2km2huq-rj.a.run.app/passenger';
+   
     constructor(private http: HttpClient) {}
 
     getPassenger(id: number): Observable<PassengerModel>{
         return this.http.get<PassengerModel>(this.url + '/get?id=' + id);
+    }
+
+    savePassenger(passenger: PassengerModel): Observable<PassengerModel>{
+        return this.http.post<PassengerModel>(this.url + '/save',passenger, httpOptions);
     }
 }
