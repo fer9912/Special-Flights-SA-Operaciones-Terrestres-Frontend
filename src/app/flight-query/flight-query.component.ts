5e2cecd13f146a7289cbe0d1e287a07441fd7254
Alejo Miño <alejomino@outlook.es>
2022-05-08 21:43:14 -0300

    flight query example v2

--- src/app/flight-query/flight-query.component.ts
+++ src/app/flight-query/flight-query.component.ts
@@ -1,22 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface Vuelo {
+  codigo: string;
+  salida: string;
+  destino: string;
+  diaSem: string;
+  hora: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const ELEMENT_DATA: Vuelo[] = [
+  { codigo: 'AA0001', salida: 'AEP', destino: 'COR', diaSem: 'Lunes' , hora:'18:50'},
+  { codigo: 'AV0204', salida: 'EZE', destino: 'MDZ', diaSem: 'Lunes' , hora:'07:45'}
 ];
 
 @Component({
@@ -25,11 +18,11 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./flight-query.component.css']
 })
 export class FlightQueryComponent implements OnInit {
-  displayedColumns: string[] = ['demo-position', 'demo-name', 'demo-weight', 'demo-symbol'];
-  dataSource = ELEMENT_DATA;  
+  displayedColumns: string[] = ['demo-codigo', 'demo-salida', 'demo-destino', 'demo-dia','demo-hora'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
   }
- 
+
 }
