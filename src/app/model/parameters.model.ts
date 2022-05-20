3fa710775c4771215e80cb1ecc35c2508eb28756
Jano Siccardi <janosiccardi99@gmail.com>
2022-05-20 16:06:45 -0300

    - nuevo popup para parametros de algoritmo

---
+++ src/app/model/parameters.model.ts
@@ -0,0 +1,8 @@
+export class ParametersModel {
+    id: number;
+    gananciaPorPersona: number;
+    costoLitroCombustible: number;
+    costoLitroLubricante: number;
+    costoInsumosPorPersona: number;
+    promedioDePersonas: number;
+  }
