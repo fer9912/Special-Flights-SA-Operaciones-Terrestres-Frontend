db699602cee0430e8f761045f483c0f6a4d59dfb
Alejo Miño <alejomino@outlook.es>
2022-06-02 20:01:28 -0300

    vuelos programados filtra por fecha

--- src/app/flight-query/flight-query.component.ts
+++ src/app/flight-query/flight-query.component.ts
@@ -11,38 +11,128 @@ export interface Vuelo {
 }
 
 const ELEMENT_DATA: Vuelo[] = [
-  { codigo: 'AA0001', salida: 'AEP', destino: 'COR', diaSem: 'Lunes', hora: '18:50' },
-  { codigo: 'AV0204', salida: 'EZE', destino: 'MDZ', diaSem: 'Lunes', hora: '07:45' },
-  { codigo: 'AV0204', salida: 'EZE', destino: 'MDZ', diaSem: 'Lunes', hora: '07:45' },
-  { codigo: 'AV0204', salida: 'EZE', destino: 'MDZ', diaSem: 'Lunes', hora: '07:45' },
-  { codigo: 'AV0204', salida: 'EZE', destino: 'MDZ', diaSem: 'Lunes', hora: '07:45' },
-  { codigo: 'AV0204', salida: 'EZE', destino: 'MDZ', diaSem: 'Lunes', hora: '07:45' },
-  { codigo: 'AV0204', salida: 'EZE', destino: 'MDZ', diaSem: 'Lunes', hora: '07:45' },
-  { codigo: 'AV0204', salida: 'EZE', destino: 'MDZ', diaSem: 'Lunes', hora: '07:45' },
-  { codigo: 'AV0204', salida: 'EZE', destino: 'MDZ', diaSem: 'Lunes', hora: '07:45' },
-  { codigo: 'AV0204', salida: 'EZE', destino: 'MDZ', diaSem: 'Lunes', hora: '07:45' },
-  { codigo: 'AV0204', salida: 'EZE', destino: 'MDZ', diaSem: 'Lunes', hora: '07:45' },
-  { codigo: 'AV0204', salida: 'EZE', destino: 'MDZ', diaSem: 'Lunes', hora: '07:45' },
-  { codigo: 'AV0204', salida: 'EZE', destino: 'MDZ', diaSem: 'Lunes', hora: '07:45' }
+  {
+    codigo: 'AA0001',
+    salida: 'AEP',
+    destino: 'COR',
+    diaSem: 'Lunes',
+    hora: '18:50',
+  },
+  {
+    codigo: 'AV0204',
+    salida: 'EZE',
+    destino: 'MDZ',
+    diaSem: 'Lunes',
+    hora: '07:45',
+  },
+  {
+    codigo: 'AV0204',
+    salida: 'EZE',
+    destino: 'MDZ',
+    diaSem: 'Lunes',
+    hora: '07:45',
+  },
+  {
+    codigo: 'AV0204',
+    salida: 'EZE',
+    destino: 'MDZ',
+    diaSem: 'Lunes',
+    hora: '07:45',
+  },
+  {
+    codigo: 'AV0204',
+    salida: 'EZE',
+    destino: 'MDZ',
+    diaSem: 'Lunes',
+    hora: '07:45',
+  },
+  {
+    codigo: 'AV0204',
+    salida: 'EZE',
+    destino: 'MDZ',
+    diaSem: 'Lunes',
+    hora: '07:45',
+  },
+  {
+    codigo: 'AV0204',
+    salida: 'EZE',
+    destino: 'MDZ',
+    diaSem: 'Lunes',
+    hora: '07:45',
+  },
+  {
+    codigo: 'AV0204',
+    salida: 'EZE',
+    destino: 'MDZ',
+    diaSem: 'Lunes',
+    hora: '07:45',
+  },
+  {
+    codigo: 'AV0204',
+    salida: 'EZE',
+    destino: 'MDZ',
+    diaSem: 'Lunes',
+    hora: '07:45',
+  },
+  {
+    codigo: 'AV0204',
+    salida: 'EZE',
+    destino: 'MDZ',
+    diaSem: 'Lunes',
+    hora: '07:45',
+  },
+  {
+    codigo: 'AV0204',
+    salida: 'EZE',
+    destino: 'MDZ',
+    diaSem: 'Lunes',
+    hora: '07:45',
+  },
+  {
+    codigo: 'AV0204',
+    salida: 'EZE',
+    destino: 'MDZ',
+    diaSem: 'Lunes',
+    hora: '07:45',
+  },
+  {
+    codigo: 'AV0204',
+    salida: 'EZE',
+    destino: 'MDZ',
+    diaSem: 'Lunes',
+    hora: '07:45',
+  },
 ];
 
 @Component({
   selector: 'app-flight-query',
   templateUrl: './flight-query.component.html',
-  styleUrls: ['./flight-query.component.css']
+  styleUrls: ['./flight-query.component.css'],
 })
 export class FlightQueryComponent implements OnInit {
-  displayedColumns: string[] = ['demo-code','demo-company','demo-day','demo-hour','demo-date', 'demo-origin', 'demo-destination', 'demo-route', 'demo-aircraft'];
+  displayedColumns: string[] = [
+    'demo-code',
+    'demo-company',
+    'demo-day',
+    'demo-hour',
+    'demo-date',
+    'demo-origin',
+    'demo-destination',
+    'demo-route',
+    'demo-aircraft',
+  ];
   dataSource = ELEMENT_DATA;
   vuelos: FlightModel[];
 
-
-  constructor(private _flightService: FlightService) { }
+  constructor(private _flightService: FlightService) {}
 
   ngOnInit(): void {
     this._flightService.getAllFlights().subscribe((response: FlightModel[]) => {
       this.vuelos = response;
+      let vuelosFiltrados = response.filter(
+        (a) => new Date(a.date) >= new Date(Date.now())
+      );
+      console.log(vuelosFiltrados);
     });
   }
-
 }
